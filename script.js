cd0b3af25dc5d3bbc11a6bcc5887586a6a347937
MD. Tanvir <mt14050@gmail.com>
2023-04-14 23:10:49 +0600

    active link design

---
+++ script.js
@@ -0,0 +1,22 @@
+const navToggler = document.querySelector('.nav-toggler');
+const navLink = document.querySelector('.nav__link');
+
+const icon = navToggler.querySelector('.fa-bars')
+//open and close function  
+navToggler.addEventListener('click', function () {
+    navLink.classList.toggle('toggler-open');
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.replace("fa-bars","fa-xmark")
+    } else {
+        icon.classList.replace("fa-xmark","fa-bars")
+    }
+})
+
+// active link
+
+const activePage = window.location.pathname;
+const Link = document.querySelectorAll('.nav__link a').forEach(link => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add("active")
+    }
+})
