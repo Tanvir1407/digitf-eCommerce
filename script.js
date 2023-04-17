4fc9423a751b5c4244ba3434a02d16466a583caf
MD. Tanvir <mt14050@gmail.com>
2023-04-18 03:23:30 +0600

    video add

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const navToggler = document.querySelector('.nav-toggler');
 const navLink = document.querySelector('.nav__link');
-
+const Play = document.getElementById("play");
+const player =document.querySelector('.video__box')
+const cancelPlayer =document.querySelector('.video__button')
 const icon = navToggler.querySelector('.fa-bars')
+let   video = document.getElementById("videoId")
 
-const Home = document.querySelector('.home');
+
+Play?.addEventListener("click", function () {
+    player.classList.add('play__Video');
+    video.src ="https://www.youtube.com/embed/elOLEDKFbf0?autoplay=1"
+})
+cancelPlayer?.addEventListener("click", function () {
+    player.classList.remove('play__Video');
+    video.src ="https://www.youtube.com/embed/elOLEDKFbf0?autoplay=0"
+})
+
+
+
+
 //open and close function  
 navToggler.addEventListener('click', function () {
     navLink.classList.toggle('toggler-open');
